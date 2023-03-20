faae45f4d40a50cdf65b8e675a0b64b61102b0f6
Seongil Yoon <56597284+seongil99@users.noreply.github.com>
2023-03-20 22:05:20 +0900

    release/main (#3)
    * Create .github/workflows/node.js.yml
    
    * Update nodejs-ci.yml
    
    * Update nodejs-ci.yml
    
    * feat: add base62 encoding
    
    * fix: cache clear
    
    * fix: change variable name
    
    * fix: delete comment
    
    * fix: hide environment variable
    
    * fix: add shell file to create db
    
    * fix: delete test
    
    ---------
    
    Co-authored-by: jsh990620 <99671891+jsh990620@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Url shortner API')
+    .setDescription('The url shortner API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
